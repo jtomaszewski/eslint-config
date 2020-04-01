aa8e268e2d8ba4792688e08765191396b8ded05b
Jacek Tomaszewski <jacek@jtom.me>
2020-04-01 16:42:27 +0200

    Initial commit: Use eslint config from twofront-landing

---
+++ index.js
@@ -0,0 +1,162 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'react-hooks',
+    'jest',
+    'promise',
+    'unicorn',
+  ],
+
+  env: {
+    browser: true,
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
+  },
+
+  rules: {
+    // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
+    'no-prototype-builtins': 'off',
+
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+
+    // TODO disable it only for contentful generated file
+    '@typescript-eslint/interface-name-prefix': 'off',
+
+    // It's OK if u know how to use it, that's why it's built-in into TS
+    '@typescript-eslint/no-non-null-assertion': 'off',
+
+    // This might be nice, but it's too late now to enforce it
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    // Makes no sense to allow type inferrence for expression parameters, but require typing the response
+    // "@typescript-eslint/explicit-function-return-type": [
+    //   "error",
+    //   { "allowExpressions": true, "allowTypedFunctionExpressions": true }
+    // ],
+
+    // TS is better than proptypes
+    'react/prop-types': 0,
+
+    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+    'react/destructuring-assignment': 'off',
+
+    // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
+    'react/jsx-filename-extension': 'off',
+
+    // It's not ideal true, but TS will error us when it's spreading too much
+    'react/jsx-props-no-spreading': 'off',
+
+    // It's good if used well (i.e. when defining a var that replaces a function argument)
+    'no-underscore-dangle': 'off',
+
+    // It's bugged now, too many false positives
+    // ( https://github.com/typescript-eslint/typescript-eslint/issues/522 )
+    '@typescript-eslint/unbound-method': 'off',
+
+    // Somehow TS doesn't infer the type for me in a code like this:
+    // ```ts
+    // const goToNextQuestion = useCallback(
+    //   (validate = true) => {
+    //     ...
+    // ```
+    '@typescript-eslint/no-inferrable-types': [
+      'warn',
+      { ignoreParameters: true },
+    ],
+
+    // Workaround for https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+
+    // With prettier around, they aren't that bad
+    'no-nested-ternary': 'off',
+    'unicorn/no-nested-ternary': 'off',
+
+    // Sometimes it's ok
+    'react/no-array-index-key': 'off',
+
+    // Use function hoisting to improve code readability
+    'no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true },
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        typedefs: true,
+      },
+    ],
+
+    // Common abbreviations are known and readable
+    'unicorn/prevent-abbreviations': 'off',
+
+    // Too lazy now to define our file naming rules in eslint
+    'unicorn/filename-case': 'off',
+
+    // Until we fix some legacy files, we'll leave that as possible
+    'unicorn/no-abusive-eslint-disable': 'off',
+
+    'unicorn/no-for-loop': 'off',
+  },
+
+  overrides: [
+    // Allow for /// import declarations in .d.ts files
+    {
+      files: '*.d.ts',
+      rules: {
+        'spaced-comment': 'off',
+      },
+    },
+    // Test files may use dev deps
+    {
+      files: ['test/**/*', '**.test.{js,jsx,ts,tsx}'],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: true,
+          },
+        ],
+      },
+    },
+  ],
+};
