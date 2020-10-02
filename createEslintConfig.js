bc311cd566505a9f07d4870ea4904ec7a7cd4071
Jacek Tomaszewski <jacek@jtom.me>
2020-10-02 16:31:28 +0200

    fixup! Ignore no-unsafe ts rules

--- createEslintConfig.js
+++ createEslintConfig.js
@@ -186,6 +186,7 @@ module.exports = function createEslintConfig({ react = false } = {}) {
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
 
       ...(react
         ? {
